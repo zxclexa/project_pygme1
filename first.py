7cb89aef8cb78da0382781cfb3946f66f256d571
mmm <93472581g@gmail.com>
2023-02-10 18:22:34 +0300

    only button

--- first.py
+++ first.py
@@ -1,1 +1,32 @@
-print('qwedasfasdfasdf')
+import sys
+from random import randrange
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+
+
+class Window(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('circles.ui', self)
+        #self.startButton.clicked.connect(self.draw_ellipse)
+        #a =
+
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        self.draw_ellipse(painter)
+        painter.end()
+    def draw_ellipse(self, painter):
+        painter.setBrush(QColor(251, 255, 31))
+        painter.drawEllipse(randrange(200), randrange(200), randrange(80), randrange(80))
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    zxc = Window()
+    zxc.show()
+    sys.exit(app.exec())
